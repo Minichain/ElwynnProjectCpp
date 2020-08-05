84bdf996a938218d8f5007cad6e2809fbb788e08
Minichain <adriacadenarubio@gmail.com>
2020-08-05 13:48:06 +0200

    - MathUtils Class refactored.

--- src/MathUtils.cpp
+++ src/MathUtils.cpp
@@ -1,13 +1,7 @@
 #include "MathUtils.h"
 
 void MathUtils::rotatePoint(float* point, float alpha, float beta, float gamma) {
-	float* vertexPointer = MathUtils::getPointRotated(point, alpha, beta, gamma);
-
-	point[0] = vertexPointer[0];
-	point[1] = vertexPointer[1];
-	point[2] = vertexPointer[2];
-
-	delete[] vertexPointer;
+	MathUtils::rotatePoint(point, new float[3] {0, 0, 0}, alpha, beta, gamma);
 }
 
 void MathUtils::rotatePoint(float* point, float* centerOfRotation, float alpha, float beta, float gamma) {
@@ -18,24 +12,6 @@ void MathUtils::rotatePoint(float* point, float* centerOfRotation, float alpha, 
 	point[2] = vertexPointer[2];
 
 	delete[] vertexPointer;
-}
-
-float* MathUtils::getPointRotated(float point[3], float alpha, float beta, float gamma) {
-	float* rotatedPoint = new float[3];
-
-	rotatedPoint[0] = point[0] * cos(alpha) * cos(beta) 
-		+ point[1] * (cos(alpha) * sin(beta) * sin(gamma) - sin(alpha) * cos(gamma))
-		+ point[2] * (cos(alpha) * sin(beta) * cos(gamma) + sin(alpha) * sin(gamma));
-
-	rotatedPoint[1] = point[0] * sin(alpha) * cos(beta)
-		+ point[1] * (sin(alpha) * sin(beta) * sin(gamma) + cos(alpha) * cos(gamma))
-		+ point[2] * (sin(alpha) * sin(beta) * cos(gamma) - cos(alpha) * sin(gamma));	
-
-	rotatedPoint[2] = - point[0] * sin(beta)
-		+ point[1] * cos(beta) * sin(gamma)
-		+ point[2] * cos(beta) * cos(gamma);
-
-	return rotatedPoint;
 }
 
 float* MathUtils::getPointRotated(float point[3], float centerOfRotation[3], float alpha, float beta, float gamma) {
